b703f1a8754f3c0ee02ec1757d2d16b281bc575a
pimp-ua <stanislav.skruzhevskiy@gmail.com>
2026-09-05 21:19:29 +0300

    v2 - обавлено удаление сделки

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ while True:
     print("""
     [1] Добавить сделку 
     [2] Показать сделки
-    [3] Выход
+    [3] Удалить сделку
+    [4] Выход
     """)
 
     user_number = input("Выбери цифру из меню: ")
@@ -13,6 +14,8 @@ while True:
     elif user_number == "2":
         deal.conclusion_get()
     elif user_number == "3":
+        deal.delete_deal()
+    elif user_number == "4":
         break
     else:
         print("ℹ️Такой цифры нет в меню")
